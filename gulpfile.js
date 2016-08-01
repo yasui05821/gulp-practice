2c45a2f5702caac28cf18bcceba3413cbc3b4762
安井 利紗 <yasui05821@gmail.com>
2016-08-01 13:32:19 +0900

    first

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require("gulp");
+var jade = require("gulp-jade");
+var sass = require("gulp-sass");
+var browser = require("browser-sync")
+
+gulp.task("default",["watch"]);
+gulp.task("build",["jade","sass"]);
+
+gulp.task("jade",function(){
+   return gulp.src(["./frontend/assets/tmpl/**/*.jade","!./frontend/assets/tmpl/**/_*.jade"])
+       .pipe(jade({
+          pretty: true
+       }))
+       .pipe(gulp.dest("./public"))
+});
+
+
+gulp.task("sass",function(){
+   gulp.src("./frontend/assets/sass/**/*.scss")
+       .pipe(sass())
+       .pipe(gulp.dest("./public/css"))
+});
+
+
+gulp.task("watch",function(){
+   gulp.watch("./frontend/assets/tmpl/**/*.jade",["jade"]);
+   gulp.watch("./frontend/assets/sass/**/*.scss",["sass"]);
+});
+
+
+gulp.task("server",function() {
+    browser.init({
+        server: {
+            baseDir:"./public"
+        },
+        open: "external"
+    });
+    gulp.watch([
+        "./public/**/*"
+    ],function(){
+        setTimeout(function(){
+            browser.reload();
+
+        },500);
+    })
+});
